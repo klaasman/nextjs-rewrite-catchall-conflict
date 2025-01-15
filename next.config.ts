8085e43edb103e66fa581656af65d8aec656e603
Klaas Landsman <mail@klaaslandsman.nl>
2025-01-15 12:26:09 +0100

    Create repro

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+
+  async rewrites() {
+    return {
+      beforeFiles: [],
+      afterFiles: [],
+      fallback: [
+        {
+          source: "/:path*",
+          destination: "https://httpstat.us/:path*",
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
